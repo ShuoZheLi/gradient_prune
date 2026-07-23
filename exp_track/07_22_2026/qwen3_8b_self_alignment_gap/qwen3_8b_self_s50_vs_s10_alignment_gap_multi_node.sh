f1b5a9068872e2ff41eb2d96857924da13301b42
ShuoZheLi <lishuozhe1999@163.com>
2026-07-22 20:15:41 -0500

    modified:   exp_track/07_22_2026/qwen3_8b_self_alignment_gap/qwen3_8b_self_dense_vs_s10_alignment_gap_multi_node.sh 	modified:   exp_track/07_22_2026/qwen3_8b_self_alignment_gap/qwen3_8b_self_s50_vs_s10_alignment_gap_multi_node.sh 	modified:   exp_track/07_22_2026/qwen3_8b_self_alignment_gap/qwen3_8b_self_s50_vs_s20_alignment_gap_multi_node.sh 	modified:   exp_track/07_22_2026/qwen3_8b_self_alignment_gap/qwen3_8b_self_s50_vs_s30_alignment_gap_multi_node.sh 	modified:   exp_track/07_22_2026/qwen3_8b_self_alignment_gap/qwen3_8b_self_s50_vs_s40_alignment_gap_multi_node.sh

--- exp_track/07_22_2026/qwen3_8b_self_alignment_gap/qwen3_8b_self_s50_vs_s10_alignment_gap_multi_node.sh
+++ exp_track/07_22_2026/qwen3_8b_self_alignment_gap/qwen3_8b_self_s50_vs_s10_alignment_gap_multi_node.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=72
-#SBATCH --time=02:00:00
+#SBATCH --time=0:00:00
 #SBATCH --output=slurm-%j_qwen3_8b_s50_vs_s10.out
 #SBATCH --error=slurm-%j_qwen3_8b_s50_vs_s10.err
 
