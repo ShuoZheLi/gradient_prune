a21c39ac1cbc5277ec9b5077f87693853e52a695
ShuoZheLi <lishuozhe1999@163.com>
2026-07-29 21:04:43 -0500

    modified:   exp_track/07_28_2026/eval_distill/sft_qwen3_1d7b_base/eval_global_step_0.sh

--- exp_track/07_28_2026/eval_distill/sft_qwen3_1d7b_base/eval_global_step_0.sh
+++ exp_track/07_28_2026/eval_distill/sft_qwen3_1d7b_base/eval_global_step_0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=eval_qwen3_1d7b_global_step_0
 #SBATCH --account=ASC26008
-#SBATCH --partition=gh
+#SBATCH --partition=gh-dev
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=72
